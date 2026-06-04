51cca67e69521cd0db9e5855d5e1db9882e9b949
Jmachado2005 <machadoj1@wit.edu>
2026-06-04 11:45:20 -0400

    Pre-master branch
    Just putting this all here before I push to the master branch

---
+++ Calculation Stuff/SolarSystem.py
@@ -0,0 +1,56 @@
+import solarsystem as ss
+import datetime
+
+now    = datetime.datetime.utcnow()
+now    = datetime.datetime.now(datetime.timezone.utc)
+
+
+
+
+def moonphase(year,month,day,hour,minute):
+    mooninfo = ss.Moon(year, month, day, hour, minute, -5, 0, -71.0571, 42.3611, True)
+    moonchange = ss.Moon(year, month, day-1, hour, minute, -5, 0, -71.0571, 42.3611, True)
+    phase = ""
+    if(mooninfo.phase() > moonchange.phase()):
+        phase = "Waxing"
+    else:
+        phase = "Waning"
+    moontype = ""
+    if(round(mooninfo.phase(),1) == 0.5):
+        moontype = "Half Moon"
+    elif(round(mooninfo.phase(),2) < 0.1):
+        moontype = "New Moon"
+    elif(round(mooninfo.phase(),2) > 0.99):
+        moontype = "Full Moon"
+    elif(round(mooninfo.phase(),2) > 0.5):
+        moontype = "Gibbous"
+    elif(round(mooninfo.phase(),2) < 0.5):
+        moontype = "Crescent"
+    phaselist = [phase, moontype]
+    return phaselist
+    #Returns an array with the whether the moon is waxing or waning, and the current phase of the moon
+
+def moon(year, month, day, hour, minute):
+    mooninfo = ss.Moon(year, month, day, hour, minute, -5, 0, -71.0571, 42.3611, True)
+    moonpos = mooninfo.position()
+    moonpos = ss.spherical2rectangular(moonpos[0],moonpos[1],moonpos[2])
+    return moonpos
+    #returns the position of the moon in rectangular coordinates
+
+def planets(year,month,day,hour,minute):
+    Helio = ss.Heliocentric(year, month, day, hour, minute, 0, 0, 'rectangular', True)
+    return Helio.planets()
+    #Returns a dictionary of planets and their rectangular coordinates (X,Y,Z)
+
+def sunriseSet(year, month, day):
+    sun = ss.Sunriseset(year, month, day, -5, 0, -71.0571, 42.3611)
+    return sun.riseset()
+
+
+
+print(moonphase(now.year,now.month,now.day,now.hour,now.minute))
+Hh = planets(now.year,now.month,now.day,now.hour,now.minute)
+for key, value in Hh.items():
+    print(f"{key},{value}")
+print(moon(now.year,now.month,now.day,now.hour,now.minute))
+print(sunriseSet(now.year,now.month,now.day))
