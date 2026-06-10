459b75f3b708edc168b9f741aa5a5ef7637c8d10
milosilverman <milo.jack.silverman@gmail.com>
2026-06-10 13:18:49 -0400

    Adding all small bodies to database now

--- SmallBodiesDBPopulation.py
+++ SmallBodiesDBPopulation.py
@@ -7,6 +7,12 @@ sql_command = """INSERT OR IGNORE INTO SmallBodies VALUES('McNaught', 'Comet', '
 cursor.execute(sql_command)
 sql_command = """INSERT OR IGNORE INTO SmallBodies VALUES('Halley', 'Comet', 'Inner and Outer Belt', 3.4, 55);"""
 cursor.execute(sql_command)
+sql_command = """INSERT OR IGNORE INTO SmallBodies VALUES('Apophis', 'Asteroid', 'Solar System', 0.185, 30.73);"""
+cursor.execute(sql_command)
+sql_command = """INSERT OR IGNORE INTO SmallBodies VALUES('Neowise', 'Comet', 'Solar System', 2.5, 1.5);"""
+cursor.execute(sql_command)
+sql_command = """INSERT OR IGNORE INTO SmallBodies VALUES('Tsuchinshan-ATLAS', 'Comet', 'Solar System', 5.9, 67.33);"""
+cursor.execute(sql_command)
 
 database.commit()
 database.close()
