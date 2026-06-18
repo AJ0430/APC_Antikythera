3f161c08d58c35af6697cce0279d2f64ce507889
aj0430 <gloryrulztherainforest@gmail.com>
2026-06-18 11:33:23 -0400

    made minor modifications

--- Calculation Stuff/SolarSystem.py
+++ Calculation Stuff/SolarSystem.py
@@ -1,6 +1,8 @@
 import solarsystem as ss
 import datetime
 from zoneinfo import ZoneInfo
+import astronomy
+import matplotlib.pyplot as plt
 
 tz = ZoneInfo("America/New_York")
 now    = datetime.datetime.now(datetime.UTC)
@@ -12,6 +14,7 @@ def daylightSavings(year, month, day, hour, minute):
     dst = f"{dst.dst()}"
     hours, minutes, seconds = map(int, dst.split(":"))
     return hours
+    
 def moonphase(year,month,day,hour,minute):
     mooninfo = ss.Moon(year, month, day, hour, minute, -5, daylightSavings(year,month,day,hour,minute), -71.0571, 42.3611, True)
     moonchange = ss.Moon(year, month, day-1, hour, minute, -5, daylightSavings(year,month,day,hour,minute), -71.0571, 42.3611, True)
@@ -50,8 +53,17 @@ def planets(year,month,day,hour,minute):
 def sunriseSet(year, month, day):
     sun = ss.Sunriseset(year, month, day, -5, daylightSavings(year,month,day,12,0), -71.0571, 42.3611)
     return sun.riseset()
+    #Returns the sunrise and sunset in decimal time
 
+def JMoons(year, month, day, hour, minute):
+    time = astronomy.Time.Make(year,month,day,hour,minute,0)
+    JMooninfo = astronomy.JupiterMoons(time)
+    return JMooninfo
+    #returns an object containing the positional coordinates of the 4 major moons of jupiter
 
+def Equinox(year):
+    return astronomy.Seasons(year)
+    #returns an object conaining the four equinoxes of a given year
 
 print(moonphase(now.year,now.month,now.day,now.hour,now.minute))
 Hh = planets(now.year,now.month,now.day,now.hour,now.minute)
@@ -59,3 +71,7 @@ for key, value in Hh.items():
     print(f"{key},{value}")
 print(moon(now.year,now.month,now.day,now.hour,now.minute))
 print(sunriseSet(now.year,now.month,now.day))
+jmoons = JMoons(now.year,now.month,now.day,now.hour,now.minute)
+eq = Equinox(2026)
+print(eq.mar_equinox)
+print(f"{jmoons}")
