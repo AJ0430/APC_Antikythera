10c814986729effad4c16b6eac72060c8c9f2a0f
milosilverman <168453123+milosilverman@users.noreply.github.com>
2026-07-09 12:02:36 -0400

    All planets populating

--- GUI_Stuff/basicGUI.py
+++ GUI_Stuff/basicGUI.py
@@ -83,13 +83,29 @@ def solarSystemView():
     my_turtle.shape("circle")
     my_turtle.color("yellow")
     mercury = Planet("Mercury", 40, 'grey')
-    solarSystem = [mercury]
+    venus = Planet("Venus",80, 'orange')
+    earth=Planet("Earth",100,'blue')
+    mars = Planet("Mars",150, 'red')
+    jupiter=Planet("Jupiter",180, 'brown')
+    saturn=Planet("Saturn",230, 'pink')
+    uranus=Planet("Uranus",250, 'light blue')
+    neptune=Planet("Neptune",280, 'black')
+    pluto=Planet("Pluto",300, 'white')
+    solarSystem = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
     
     while True: #placeholder to calculate angle based on date entered
         canvas.update()
         for i in solarSystem:
             i.move()
         mercury.angle += 0.05
+        venus.angle += 0.03
+        earth.angle += 0.01
+        mars.angle += 0.007
+        jupiter.angle += 0.02
+        saturn.angle += 0.018
+        uranus.angle += 0.016
+        neptune.angle += 0.005
+        pluto.angle += 0.003
     
 # creating main window
 root = tk.Tk()
