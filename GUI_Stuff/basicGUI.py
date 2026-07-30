2dc9f3eeff87b235063b0961ff3b98a4131068e4
milosilverman <168453123+milosilverman@users.noreply.github.com>
2026-07-30 12:04:48 -0400

    Moved command order on date select, solar system command was blocking small bodies

--- GUI_Stuff/basicGUI.py
+++ GUI_Stuff/basicGUI.py
@@ -168,7 +168,7 @@ def dateSelectionFixed():
                     print("Error Importing image please try again")
 
 
-    show_btn = tk.Button(root, text="Confirm Date", command=lambda:(show_selection(), show_EclipseMenu(), solarSystemView(), show_smallbody()))
+    show_btn = tk.Button(root, text="Confirm Date", command=lambda:(show_selection(), show_EclipseMenu(), show_smallbody(), solarSystemView()))
     show_btn.place(x = 1210, y = 30)
 
     root.mainloop()
