c2a99437d8818345a1af873cb3322255688fb0e7
Jmachado2005 <machadoj1@wit.edu>
2026-08-06 10:25:42 -0400

    quick additions I made for the manual

--- APC Project.py
+++ APC Project.py
@@ -1,5 +1,11 @@
+# ELEC3225 Applied Programming Concepts - Large Group Project (Replicate Antikythera)
+# Professor Rawlins
+# 8/6/2026
+
+# Project Members:
+# Harrison Brown, Rafael Enamorado, Joe Machado, Milo Silverman, Ashton Vallejo, & David Vozzo
+
+# Main file for the project. This file will be used to run the GUI
 from GUI_Stuff import basicGUI
-# This will become our "main" file
-# We will import our other files here and run the program from this file
-# Leave blank for now until we have the other files created
+
 
